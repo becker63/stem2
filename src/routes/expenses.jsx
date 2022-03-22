1403641e2a52130123b70c737362dec17f992932
Becker63 <johnsontaylor6320@gmail.com>
2022-03-22 07:10:26 -0400

    first commit

---
+++ src/routes/expenses.jsx
@@ -0,0 +1,10 @@
+import AppBar from "../components/AppBar";
+
+export default function expenses() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <AppBar />
+        <h2>expenses</h2>
+      </main>
+    );
+  }
